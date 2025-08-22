fdf691d094164d9f81a4c64076e62723331cd07c
Ángel-lop <alopezh29@gmail.com>
2025-08-21 21:55:48 -0600

    Comentario de migracion User

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration
 {
     /**
-     * Run the migrations.
+     * Migracion de la tablas para base de datos.
      */
     public function up(): void
     {
