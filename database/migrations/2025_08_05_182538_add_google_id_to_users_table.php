2e3f50925df7c009b15e8c4a30a37d0e159e4a22
Ángel-lop <alopezh29@gmail.com>
2025-08-22 17:59:37 -0600

    Solucion de Registros de Inicios en Google

--- database/migrations/2025_08_05_182538_add_google_id_to_users_table.php
+++ database/migrations/2025_08_05_182538_add_google_id_to_users_table.php
@@ -1,5 +1,5 @@
 <?php
-
+# Este apartado soluciona parde del bug de inicio de google
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
